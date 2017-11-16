357654438d6bdc173cc47acdff4eaee9204b0db1
Brecht Philips <brecht.philips@student.pxl.be>
2017-11-16 17:11:38 +0100

    bug fix

--- WP2/wp2/src/statuses/status-add.page.js
+++ WP2/wp2/src/statuses/status-add.page.js
@@ -7,49 +7,45 @@ import imageGreen from "../images/green-smiley.png";
 import imageRed from "../images/red-smiley.png";
 import imageLightRed from "../images/lightred-smiley.png";
 import {Col, Grid, Row} from "react-bootstrap";
-import {Snackbar} from "material-ui";
+import {Redirect} from "react-router";
 
 class StatusAddPage extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            open: false,
+            submit:false
         };
     }
 
-    handleRequestClose = () => {
-        this.setState({
-            open: false,
-        });
-    };
-
     render() {
-        return (
-            <Grid>
-                <Row style={{marginTop: '30px'}}>
-                    <Col md={4} lg={4}>
-                        <Image src={imageGreen} alt="HAPPY" name={"HAPPY"}
-                               onClick={() => this.save("HAPPY")} color="white" style={{cursor: 'pointer'}}/>
-                    </Col>
-                    <Col md={4} lg={4}>
-                        <Image src={imageLightRed} alt="MEDIUM" name={"MEDIUM"}
-                               onClick={() => this.save("MEDIUM")} color="white" style={{cursor: 'pointer'}}/>
-                    </Col>
-                    <Col md={4} lg={4}>
-                        <Image src={imageRed} alt="MAD" name={"MAD"} onClick={() => this.save("MAD")}
-                               color="white" style={{cursor: 'pointer'}}/>
-                    </Col>
-                </Row>
-                <Snackbar open={this.state.open} message="Status added!" autoHideDuration={4000}
-                          onRequestClose={this.handleRequestClose}/>
-            </Grid>
-        );
+        if (this.state.submit === true) {
+            return (<Redirect to="/"/>);
+        } else {
+            return (
+                <Grid>
+                    <Row style={{marginTop: '30px'}}>
+                        <Col md={4} lg={4}>
+                            <Image src={imageGreen} alt="HAPPY" name={"HAPPY"}
+                                   onClick={() => this.save("HAPPY")} color="white" style={{cursor: 'pointer'}}/>
+                        </Col>
+                        <Col md={4} lg={4}>
+                            <Image src={imageLightRed} alt="MEDIUM" name={"MEDIUM"}
+                                   onClick={() => this.save("MEDIUM")} color="white" style={{cursor: 'pointer'}}/>
+                        </Col>
+                        <Col md={4} lg={4}>
+                            <Image src={imageRed} alt="MAD" name={"MAD"} onClick={() => this.save("MAD")}
+                                   color="white" style={{cursor: 'pointer'}}/>
+                        </Col>
+                    </Row>
+                </Grid>
+            );
+        }
     }
 
     save = (text) => {
         const mood = text;
-        const location_id = parseInt(this.props.selectedRow, 10);
+        const location_id = parseInt(this.props.location_id, 10);
         const offset = (new Date()).getTimezoneOffset() * 60000;
         const date = new Date(Date.now() - offset).toISOString().slice(0, 19).replace('T', ' ');
         const status = {
@@ -58,7 +54,7 @@ class StatusAddPage extends Component {
             "date": date
         };
         HttpService.addStatus(status);
-        this.setState({open: true});
+        this.setState({submit: true});
     };
 
     componentDidMount() {
@@ -68,7 +64,7 @@ class StatusAddPage extends Component {
 
 const mapStateToProps = (state, ownProps) => {
     return {
-        selectedRow: state.selectedRow,
+        location_id: state.location_id,
     };
 };
 
