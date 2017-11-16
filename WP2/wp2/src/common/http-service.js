2a736f31161cd71ba53ec2704eb420532939b729
Ybe Spapen <11401526@student.pxl.be>
2017-11-16 12:38:38 +0100

    TechnicianTable naar functional component

--- WP2/wp2/src/common/http-service.js
+++ WP2/wp2/src/common/http-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 class HttpService {
-    baseUrl = 'http://192.168.46.137/api';
+    baseUrl = 'http://localhost/api';
 
     getLocations() {
         return axios.get(`${this.baseUrl}/location`).then(r => r.data);
